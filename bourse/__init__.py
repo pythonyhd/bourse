045577f57fc5ff5bb2a4729b216e1a8a2bf99780
admin_huadong <18353626676@163.com>
2020-01-13 15:10:16 +0800

    更新日志

--- bourse/__init__.py
+++ bourse/__init__.py
@@ -1,4 +1,4 @@
 import logging
 
-logging.getLogger('pdfminer').setLevel(logging.ERROR)
+logging.getLogger('pdfminer3k').setLevel(logging.ERROR)
 logging.getLogger('elasticsearch').setLevel(logging.ERROR)
